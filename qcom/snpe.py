890a806a66f6fcd5d97200c16d6f673c3b825d0c
Ubaydullo Asatullaev <Ubaydullohazard@gmail.com>
2023-08-01 15:39:53 +0900

    Update snpe.py

--- qcom/snpe.py
+++ qcom/snpe.py
@@ -12,7 +12,6 @@ cd /opt/qcom/aistack/snpe/2.12.0.230626/bin
 sudo apt update
 sudo apt install python3-pip
 check-linux-dependency.sh
-
 check-python-dependency
 # if errors encounters (python setup.py), fariler error code 1 in MarkupSafe
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -36,8 +35,6 @@ cd /opt/qcom/aistack/snpe/2.12.0.230626/bin/x86_64-linux-clang
 ./snpe-net-run
 # issue Add the directory containing libSNPE.so to the LD_LIBRARY_PATH environment variable
 sudo find / -name "libSNPE.so" 2>/dev/null
-export LD_LIBRARY_PATH=/opt/qcom/aistack/snpe/2.12.0.230626/lib/x86_64-linux-clang:$LD_LIBRARY_PATH
-# 
-
-
+export LD_LIBRARY_PATH=/opt/qcom/aistack/snpe/2.12.0.230626/lib/x86_64-linux-clang:$LD_LIBRARY_PATH 
+# issue - ./snpe-net-run: /lib/x86_64-linux-gnu/libm.so.6: version `GLIBC_2.29' not found 
 
