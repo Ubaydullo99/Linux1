495fe192db9a81ce3592e6f0c196b1e29ec56408
Ubaydullo Asatullaev <Ubaydullohazard@gmail.com>
2023-08-01 10:18:51 +0900

    Update terminanal.py

--- open/terminanal.py
+++ open/terminanal.py
@@ -2,3 +2,5 @@ wsl --list --online  # check available linux env for windows
 wsl --install -d Ubuntu-18.04 #isntalling ubuntu on windows
 
 wsl ~ -u root # enter to root
+# https://learn.microsoft.com/en-us/windows/wsl/install
+
